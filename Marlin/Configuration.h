ebef0f3d8ba8db0ec8f0fcb933d7083fbb4d2a5d
Jonas Körner <jkoerner@odd-it.com>
2019-08-12 18:31:50 +0200

    Fixed step Y

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,7 +608,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 200, 1600, 95 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 95 }
 
 /**
  * Default Max Feed Rate (mm/s)
