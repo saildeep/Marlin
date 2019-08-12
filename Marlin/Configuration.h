e461ce0eff6c8cb6e3cd11a70abc0eb071a1586a
Jonas Körner <jkoerner@odd-it.com>
2019-08-12 18:57:13 +0200

    Value stuff

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -812,7 +812,7 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   2 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES  1.5 // Z Clearance between probe points
+#define Z_CLEARANCE_BETWEEN_PROBES  2.1 // Z Clearance between probe points
 #define Z_CLEARANCE_MULTI_PROBE     .5 // Z Clearance between multiple probes
 //#define Z_AFTER_PROBING           5 // Z position after probing is done
 
@@ -867,7 +867,7 @@
 
 //#define UNKNOWN_Z_NO_RAISE // Don't raise Z (lower the bed) if Z is "unknown." For beds that fall when Z is powered off.
 
-#define Z_HOMING_HEIGHT 4  // (in mm) Minimal z height before homing (G28) for Z clearance above the bed, clamps, ...
+#define Z_HOMING_HEIGHT 2  // (in mm) Minimal z height before homing (G28) for Z clearance above the bed, clamps, ...
                              // Be sure you have this distance over your Z_MAX_POS in case.
 
 // Direction of endstops when homing; 1=MAX, -1=MIN
