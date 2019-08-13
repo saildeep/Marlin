bb5b57caee1e9852f5181bb2cbe28d7f04a76858
Jonas Körner <jkoerner@odd-it.com>
2019-08-13 19:48:43 +0200

    Changed probe offset position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -777,7 +777,7 @@
  *    (0,0)
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -30  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER -30  // Y offset: -front +behind [the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 30  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -3.75   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
