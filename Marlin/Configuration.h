024a5d43a353727062769ffff5235eb8a04ea36d
Jonas Körner <jkoerner@odd-it.com>
2019-08-14 23:12:37 +0200

    Adjusted BED pid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -431,9 +431,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 100
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 305.58
+  #define DEFAULT_bedKi 39.26
+  #define DEFAULT_bedKd 594.54
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
