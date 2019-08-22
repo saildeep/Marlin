838caffc56748892139276e78abc8a2793dd4776
Jonas Körner <jkoerner@odd-it.com>
2019-08-22 17:34:51 +0200

    Increased feedrate for slow probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -790,7 +790,7 @@
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
 
 // Feedrate (mm/m) for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW 30
+#define Z_PROBE_SPEED_SLOW 100
 
 // The number of probes to perform at each point.
 //   Set to 2 for a fast/slow probe, using the second probe result.
