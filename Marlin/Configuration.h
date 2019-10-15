286f1954b4fad4be8c854518ca09ac3538d68430
Jonas Körner <jonas.koerner@uni-konstanz.de>
2019-10-15 17:41:32 +0200

    steps per mm adjustmend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -626,7 +626,7 @@
 
 // ANET A6 Firmwae V2.0 defaults: (steps/mm)
 // Xsteps/mm: +100.0, Ysteps/mm: +100.0, Zsteps/mm: +0400.0, eSteps/mm: +0095.0
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {3200,  200, 1600,5}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {800,  100, 400,5}
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,  80, 400, 95}
 
 /**
