7e24f1e1a74ee90bf6a54bfaeea6d6cdf172d791
Jonas Körner <jkoerner@odd-it.com>
2019-11-06 10:45:32 +0100

    Switched to software spi for tmc2130

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1140,10 +1140,10 @@
    * The default SW SPI pins are defined the respective pins files,
    * but you can override or define them here.
    */
-  //#define TMC_USE_SW_SPI
-  //#define TMC_SW_MOSI       -1
-  //#define TMC_SW_MISO       -1
-  //#define TMC_SW_SCK        -1
+  #define TMC_USE_SW_SPI
+  #define TMC_SW_MOSI       65
+  #define TMC_SW_MISO       66
+  #define TMC_SW_SCK        44
 
   /**
    * Use Trinamic's ultra quiet stepping mode.
